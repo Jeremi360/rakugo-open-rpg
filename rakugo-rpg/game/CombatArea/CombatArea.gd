ce14df786e5333bcb1b9b35958050e1d604514f3
Jeremi <jeremi360@gmail.com>
2020-07-19 16:48:07 +0200

    UI almost finished

--- rakugo-rpg/game/CombatArea/CombatArea.gd
+++ rakugo-rpg/game/CombatArea/CombatArea.gd
@@ -1,8 +1,41 @@
 extends RakugoControl
 
+export var max_slots := 3
+export var spawn_parent_path : NodePath
 
 func start_combat():
-	$YSort.prepare()
 	Rakugo.hide("InGameGUI")
-	var heroes : Array = Rakugo.get_value("party") 
-	$CombatPanel._set_hero(heroes[0])
+	var party : = spawn_party(Rakugo.get_value("party"))
+	var enemies : = spawn_enemies(Rakugo.get_value("enemies"))
+	$CombatPanel.current_hero = party[0]
+
+
+func get_spawn_point(type:String, id:int) -> Node:
+	return get_node(str(spawn_parent_path) + "/" + type + str(id))
+
+func spawn(type:String, array:Array) -> Array:
+	var arr := []
+
+	for j in range(max_slots):
+		var s = get_spawn_point(type, j)
+		s.hide()
+	
+	for i in range(array.size()):
+		var p:Node2D = array[i].instance()
+		var s = get_spawn_point(type, i)
+		s.add_child(p)
+		var rpg_ch : RPGCharacter = p.get_node("RPGCharacter")
+		s.setup(rpg_ch)
+		s.show()
+		arr.append(rpg_ch)
+	
+	return arr
+
+
+func spawn_party(array:Array) -> Array:
+	return spawn("Hero", array)
+
+
+func spawn_enemies(array:Array) -> Array:
+	return spawn("Enemy", array)
+
--- rakugo-rpg/game/CombatArea/CombatPanel.gd
+++ rakugo-rpg/game/CombatArea/CombatPanel.gd
@@ -6,12 +6,16 @@ export var skill_buttons_parent_path: NodePath
 onready var skill_buttons_parent: Container = get_node(skill_buttons_parent_path)
 
 var current_hero: RPGCharacter setget _set_hero
-var _current_hero: RPGCharacter
+var _hero: RPGCharacter
 
 
 func _set_hero(value: RPGCharacter) -> void:
-	_current_hero = value
+	_hero = value
 	_on_Attack_toggled(true)
+
+
+func _get_hero() -> RPGCharacter:
+	return _hero
 
 
 func make_buttons(attack_skills: Dictionary, unlocked_skills: Array, limit := 0):
@@ -23,28 +27,35 @@ func make_buttons(attack_skills: Dictionary, unlocked_skills: Array, limit := 0)
 			var cost: int = attack_skills[s]
 			var b: AttackButton = attack_button.instance()
 			b.set_attack(s, cost)
-			skill_buttons_parent.add(b)
+			skill_buttons_parent.add_child(b)
 			b.disabled = limit > 0 and cost > limit
 
 
 func _on_Attack_toggled(button_pressed):
 	if button_pressed:
-		make_buttons(_current_hero.attack_skills, _current_hero.unlocked_skills)
+		make_buttons(
+			_hero.attack_skills, 
+			_hero.unlocked_skills)
 
 
 func _on_Magic_toggled(button_pressed):
 	if button_pressed:
-		make_buttons(_current_hero.magic_skills, _current_hero.unlocked_skills, _current_hero.mana)
+		make_buttons(
+			_hero.magic_skills,
+			_hero.unlocked_skills,
+			_hero.mana.value)
 
 
 func _on_Special_toggled(button_pressed):
 	if button_pressed:
-		make_buttons(_current_hero.special_skills, _current_hero.unlocked_skills)
+		make_buttons(
+			_hero.special_skills,
+			_hero.unlocked_skills)
 
 
 func _on_Defense_pressed():
-	_current_hero.use_skill("defense", "")
+	_hero.use_skill("defense", "")
 
 
 func _on_Flee_pressed():
-	_current_hero.use_skill("flee", "")
+	_hero.use_skill("flee", "")
